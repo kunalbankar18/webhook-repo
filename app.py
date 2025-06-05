58a59252edd41f9a1e477becd5c8f20bab05463d
kunalbankar18 <kunalbankar071@gmail.com>
2025-06-05 15:05:53 +0530

    updated code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
-from datetime import datetime
+from datetime import datetime,timezone
 import os
 
 app = Flask(__name__)
@@ -14,17 +14,40 @@ def index():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.json
-    event = {
-        "request_id": data.get("request_id"),
-        "author": data.get("author"),
-        "action": data.get("action"),
-        "from_branch": data.get("from_branch"),
-        "to_branch": data.get("to_branch"),
-        "timestamp": datetime.utcnow().strftime("%d %B %Y - %I:%M %p UTC")
+    payload = request.get_json()
+    event = request.headers.get('X-GitHub-Event')
+
+    print(f"Received event: {event}")
+    print(payload)
+
+    data = {
+        "request_id": request.headers.get('X-GitHub-Delivery'),
+        "author": None,
+        "action": None,
+        "from_branch": None,
+        "to_branch": None,
+        "timestamp": datetime.now(timezone.utc).strftime("%d %B %Y - %I:%M %p UTC")
     }
-    collection.insert_one(event)
-    return jsonify({"message": "Event stored"}), 200
+
+    if event == "push":
+        data["author"] = payload.get("pusher", {}).get("name")
+        data["action"] = "push"
+        data["to_branch"] = payload.get("ref", "").split("/")[-1]
+
+    elif event == "pull_request":
+        pr = payload.get("pull_request", {})
+        data["author"] = pr.get("user", {}).get("login")
+        data["from_branch"] = pr.get("head", {}).get("ref")
+        data["to_branch"] = pr.get("base", {}).get("ref")
+        is_merged = pr.get("merged", False)
+
+        if payload.get("action") == "closed" and is_merged:
+            data["action"] = "MERGE"
+        else:
+            data["action"] = payload.get("action").upper()
+
+    collection.insert_one(data)
+    return jsonify({"status": "success"}), 200
 
 @app.route('/events', methods=['GET'])
 def get_events():
